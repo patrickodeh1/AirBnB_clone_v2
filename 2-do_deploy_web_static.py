8852970101a784a15388c09f9bd242fe458f3208
Patrick Odeh <vyltyan@gmail.com>
2024-07-18 21:57:41 +0100

    pack

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from fabric import task, Connection
+import os
+
+# Define the hosts for deployment
+env_hosts = ['100.26.227.236', '54.197.106.162']
+
+@task
+def do_deploy(c, archive_path):
+    """
+    Distributes an archive to the web servers.
+    Returns True if all operations have been done correctly, otherwise returns False.
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        archive_file = os.path.basename(archive_path)
+        archive_name = os.path.splitext(archive_file)[0]
+
+        for host in env_hosts:
+            c = Connection(host)
+            c.put(archive_path, f"/tmp/{archive_file}")
+
+            release_dir = f"/data/web_static/releases/{archive_name}/"
+            c.run(f"mkdir -p {release_dir}")
+
+            c.run(f"tar -xzf /tmp/{archive_file} -C {release_dir}")
+
+            c.run(f"rm /tmp/{archive_file}")
+
+            c.run(f"mv {release_dir}web_static/* {release_dir}")
+
+            c.run(f"rm -rf {release_dir}web_static")
+
+            c.run("rm -rf /data/web_static/current")
+
+            c.run(f"ln -s {release_dir} /data/web_static/current")
+
+            print(f"New version deployed on {host}!")
+
+        return True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
---
+++ web_static/README.md
@@ -0,0 +1,3 @@
+## 0x01. AirBnB clone - Web static
+
+Html and css 
