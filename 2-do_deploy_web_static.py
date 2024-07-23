03b05eff8e04416702fc75a35855aed1642e6633
Patrick Odeh <vyltyan@gmail.com>
2024-07-23 19:02:10 +0100

    2-deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,7 @@
 import os
 from fabric.api import *
 
-
 env.hosts = ['100.26.227.236', '54.197.106.162']
-
 
 def do_deploy(archive_path):
     """
@@ -14,7 +12,7 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        file_name = archive_path.split(".")
+        file_name = os.path.basename(archive_path)
         no_ext = file_name.split(".")[0]
         release_path = "/data/web_static/releases/"
 
@@ -27,6 +25,6 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(release_path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return False
-    
