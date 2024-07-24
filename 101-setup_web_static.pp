2f9bf42233cdaac840f4618d5274bc0067b49375
Patrick Odeh <vyltyan@gmail.com>
2024-07-24 08:58:43 +0100

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,57 +1,54 @@
-# Define necessary directories
-file { 'versions':
+# Create the /data directory
+file { '/data':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu',
   mode   => '0755',
 }
 
-# Ensure web_static directory exists
-file { 'web_static':
+# Create the /data/web_static directory
+file { '/data/web_static':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu',
   mode   => '0755',
 }
 
-# Create a shell script to pack web_static into a .tgz archive
-file { '/usr/local/bin/do_pack.sh':
+# Create the /data/web_static/releases directory
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+# Create the /data/web_static/releases/test directory
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+}
+
+# Create a sample index.html file in /data/web_static/releases/test
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'file',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  mode    => '0755',
-  content => @("EOF")
-#!/bin/bash
-
-# Create versions directory if it doesn't exist
-if [ ! -d "versions" ]; then
-  mkdir versions
-fi
-
-# Create a timestamp
-timestamp=$(date +'%Y%m%d%H%M%S')
-
-# Create the archive name
-archive_name="versions/web_static_${timestamp}.tgz"
-
-# Print the packing message
-echo "Packing web_static to ${archive_name}"
-
-# Create the archive
-tar -cvzf ${archive_name} web_static
-
-if [ $? -eq 0 ]; then
-  echo "Archive created successfully at ${archive_name}"
-else
-  echo "An error occurred during archive creation"
-fi
-  | EOF
+  mode    => '0644',
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
 }
 
-# Execute the script to create the archive
-exec { 'pack_web_static':
-  command     => '/usr/local/bin/do_pack.sh',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  refreshonly => true,
-  subscribe   => File['/usr/local/bin/do_pack.sh'],
+# Create a symbolic link /data/web_static/current that points to /data/web_static/releases/test
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
